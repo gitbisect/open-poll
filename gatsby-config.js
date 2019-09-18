484867f5ef7da1e4ae2708e88134051f601eaa97
John Varghesee <johnvarghesee@rachels-ipro.lan>
2019-09-17 23:09:55 -0700

    Revert "fix: remove unwanted pages and files to reduce clutter"
    This reverts commit 4ed428252c95dee6fb952336be873b07e476f3d1.

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `Open Poll - just for ACD 2020`,
+    description: `Allow users to provide one or more answers to a question and vote on them. Use email ID or cookie to prevent multiple votes later`,
+    author: `John Varghese`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
